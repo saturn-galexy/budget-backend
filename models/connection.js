5aca719bfc185fc0d59a2203913719d06c338eed
giselaag <gisela.anaya.g@gmail.com>
2026-05-20 21:48:48 +0200

    add connection error logging

--- models/connection.js
+++ models/connection.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const connectionString = process.env.CONNECTION_STRING;
 
+if (!connectionString) {
+  console.error('❌ CONNECTION_STRING is undefined');
+}
+
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log('Database connected'))
-  .catch(error => console.error(error));
+  .then(() => console.log('✅ Database connected'))
+  .catch(error => console.error('❌ MongoDB connection error:', error.message));
